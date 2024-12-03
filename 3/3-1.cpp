7393c418f118d1d20ac42b74768cb67154b63f2b
SamuelSandlund <samuel.j.sandlund@gmail.com>
2024-12-03 19:57:20 +0100

    3-1 solved

--- 3/3-1.cpp
+++ 3/3-1.cpp
@@ -8,24 +8,39 @@ using namespace std;
 int main(){
     ifstream input("input.txt");
     string line;
+    int total = 0;
     size_t mul, comma, end;
     while(getline(input, line)){
-        //size_t lineLength = line.size();
         mul = line.find("mul(");
         while(mul != string::npos){
-            cout << line.substr(mul, 4) << "[AT: " << mul << "]";
             comma = line.find(",", (mul));
             if(comma != string::npos){
-
-                cout << line.substr(mul+4,3);
+                if(comma-(mul+4) > 3){
+                    //look for next mul
+                    mul = line.find("mul(", (mul+4));
+                    continue;
+                }
             }
             end = line.find(")", comma);
             if(end != string::npos){
-                cout << line.at(end) << " ";
+                if(end-comma-1 > 3){
+                    mul = line.find("mul(", (mul+4));
+                    continue;
+                }
             }
+            cout << line.substr(mul, (end-mul+1)) << '\n';
+
+            //if correct format has been confirmed parse out numbers
+            int num1, num2;
+            num1 = stoi(line.substr(mul+4,(comma-(mul+4))));
+            num2 = stoi(line.substr(comma+1, (end-comma-1)));
+            cout << num1 << " " << num2 << '\n';
             mul = line.find("mul(", end);
+
+            total = total + (num1*num2);
         }
         cout << '\n';
     }
+    cout << "ANSWER: " << total;
     return 0;
 }
